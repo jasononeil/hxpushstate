bc063c1929ac45d8cf3660fa3e874783687b8479
Allan Dowdeswell <secrets@confidant.ca>
2018-07-20 20:11:24 -0600

    Add index.php for Apache server

---
+++ demo/build/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PushState Test</title>
+	<script type="text/javascript" src="test.js"></script>
+	<link rel="stylesheet" href="bootstrap.css">
+</head>
+<body class="container">
+
+<h1>PushState Test</h1>
+
+<ul>
+	<li><a href="/muslim" rel="pushstate">Muslim</a></li>
+	<li><a href="/christian" rel="pushstate">Christian</a></li>
+	<li><a href="/jedi" class="pushstate">Jedi</a></li>
+	<li><a href="/giaraffe">Giaraffe <i>(non push state link)</i></a></li>
+	<li><a href="http://google.com">Google <i>(external link)</i></a></li>
+</ul>
+
+<form method="post" action="/custom" class="inline-form pushstate">
+	<label>Favourite Animal</label>
+	<input type="text" name="animal" value="elephant" />
+	<button type="submit" class="btn">Submit</button>
+</form>
+
+<form method="post" action="/custom" class="inline-form pushstate">
+	<label>Upload Photo</label>
+	<input type="file" name="photo" id="photo" accept=".jpg,.jpeg,.gif,.png" />
+	<button type="submit" class="btn">Upload</button>
+</form>
+
+<p>
+	<button class="btn" id="toggle-preventer">Use Preventer</button>
+</p>
+
+<div id="content" class="well">
+
+</div>
+
+<div id="load-type" class="label">This content was a page-reload</div>
+
+</body>
+</html>
